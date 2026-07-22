aa89ba4c5f2dc2ad34f50374824c7835aa4af6f3
alacan-2024010 <alacan-2024010@kinal.edu.gt>
2026-07-21 23:58:40 -0600

    Se arreglo una importacion

--- Portafolio/src/features/home/pages/HomePage.jsx
+++ Portafolio/src/features/home/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { PortafolioPage } from '../../portafolio/pages/PortafolioPage/PortafolioPage';
 import fotoPerfil from '../../../assets/FotografiaPrincipal.png';
-import '../../../styles/Home.css';
+import "../../../styles/Home.css"
 
 const TIEMPO_SIMBOLO_MS = 1500;
 const TIEMPO_TOTAL_BIENVENIDA_MS = 8000;
